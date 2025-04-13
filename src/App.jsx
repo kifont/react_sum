5b21c35eef0b5ee8645c9487504f76701b193b11
Anton Kryzhanivskyi <firechant2@gmail.com>
2025-04-14 00:10:10 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ export const App = () => (
     <Sum a={-5} b={5} />
     <Sum a={10} />
     <Sum b={5} />
+    <Sum />
     {/* Replace paragraphs with Sum componets */}
     {/* And remove commented lines :) */}
   </>
